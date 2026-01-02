22a80c203ab920888b457448b7ad09a20f80405a
Henrique Ferreira <henriquecesararaujoferreira@gmail.com>
2026-01-02 16:46:15 -0300

    update

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import PropertyDetails from './components/PropertyDetails';
 import PropertyCatalog from './components/PropertyCatalog';
